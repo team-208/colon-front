020291f70dae56a66a0eed377648269311f7b214
ohdal <yoonhj97@naver.com>
2024-01-18 16:42:40 +0900

    - feat: atom set 예제 작성

--- app/components/sample/innerPage/UserInfoBox.tsx
+++ app/components/sample/innerPage/UserInfoBox.tsx
@@ -1,11 +1,17 @@
 'use client';
-import React from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { useCallback } from 'react';
+import { useRecoilValue, useRecoilState, useSetRecoilState } from 'recoil';
 import { userState, userTagText } from '@/app/recoils/sample/user';
 
 export default function UserInfoBox() {
+  // const [user, setUser] = useRecoilState(userState);
   const user = useRecoilValue(userState);
+  const setUser = useSetRecoilState(userState);
   const tagText = useRecoilValue(userTagText);
+
+  const changeName = useCallback(() => {
+    setUser({ ...user, name: '홍길똥' });
+  }, []);
 
   return (
     <div>
@@ -13,6 +19,7 @@ export default function UserInfoBox() {
         이름: {user.name} ({user.username})
       </p>
       <p>태그: {tagText}</p>
+      <button onClick={changeName}>이름 변경</button>
     </div>
   );
 }
