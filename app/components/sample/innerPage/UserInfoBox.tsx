9fb6d26dd4325f6cfb149105356401b55d77cfd0
ohdal <yoonhj97@naver.com>
2024-01-27 20:37:36 +0900

    - feat: 비동기 예제 추가

--- app/components/sample/innerPage/UserInfoBox.tsx
+++ app/components/sample/innerPage/UserInfoBox.tsx
@@ -1,25 +1,70 @@
 'use client';
-import React, { useCallback } from 'react';
-import { useRecoilValue, useRecoilState, useSetRecoilState } from 'recoil';
-import { userState, userTagText } from '@/app/recoils/sample';
+import React, { useCallback, useEffect } from 'react';
+import { useRecoilValue, useRecoilState, useSetRecoilState, useRecoilValueLoadable } from 'recoil';
+import { userState, userTagText, userQuestionList, QuestionList } from '@/app/recoils/sample';
+
+//4. 비동기 처리 selector
+// https://stackoverflow.com/questions/76849958/recoil-async-selector-does-not-work-with-next-js
+type QuestionListComponentProps = { userID: number };
+const QuestionListComponent = ({ userID }: QuestionListComponentProps) => {
+  const { state, contents } = useRecoilValueLoadable(userQuestionList(userID));
+
+  switch (state) {
+    case 'loading':
+      return <div>loading...</div>;
+      break;
+    case 'hasValue':
+      return (
+        <div>
+          {(contents as QuestionList).map((v) => (
+            <p key={v.id}>{v.title}</p>
+          ))}
+        </div>
+      );
+      break;
+    case 'hasError':
+      return <div>데이터 불러오기에 실패했습니다.</div>;
+      break;
+  }
+};
+
+const paddingStyle = { padding: '10px 0' };
 
 export default function UserInfoBox() {
-  // const [user, setUser] = useRecoilState(userState);
-  const user = useRecoilValue(userState);
-  const setUser = useSetRecoilState(userState);
+  // 1. useRecoilState 를 이용한 get, set
+  const [user, setUser] = useRecoilState(userState);
+
+  //2. userRecoilValue, userSetRecoilState를 이용한 각각의 get, set
+  // const user = useRecoilValue(userState);
+  // const setUser = useSetRecoilState(userState);
+
+  //3. 파생 데이터 사용하기
   const tagText = useRecoilValue(userTagText);
 
   const changeName = useCallback(() => {
-    setUser({ ...user, name: '홍길똥' });
+    // 사용자 닉네임 변경 API 호출 로직
+
+    setUser((cur) => {
+      return { ...cur, username: '베트맨' };
+    });
   }, []);
 
   return (
     <div>
-      <p>
-        이름: {user.name} ({user.username})
-      </p>
-      <p>태그: {tagText}</p>
-      <button onClick={changeName}>이름 변경</button>
+      <div style={paddingStyle}>
+        <h3>기본 정보</h3>
+        <p>
+          이름: {user.name} ({user.username})
+        </p>
+        <p>태그: {tagText}</p>
+        <button onClick={changeName}>닉네임 변경</button>
+      </div>
+      <div style={paddingStyle}>
+        <h3>나의 질문 리스트</h3>
+        {/* <React.Suspense fallback={<div>Loading...</div>}> */}
+        <QuestionListComponent userID={user.id} />
+        {/* </React.Suspense> */}
+      </div>
     </div>
   );
 }
--- app/recoils/sample/user/atom.ts
+++ app/recoils/sample/user/atom.ts
@@ -4,7 +4,7 @@ import { atom } from 'recoil';
 export const userState = atom({
   key: 'userState',
   default: {
-    id: '14032',
+    id: 14032,
     name: '홍길동',
     username: '슈퍼맨',
     tagList: ['프론트엔드', 'ReactJS', 'NextJS', 'Javascript', 'HTML', 'CSS'],
