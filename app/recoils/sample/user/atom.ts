7f7ed649ce593cee74e9621e23a2b57d65f19ee5
ohdal <yoonhj97@naver.com>
2024-02-02 17:16:28 +0900

    - feat: localstorage 저장 atom effects 예제 추가

--- app/recoils/sample/user/atom.ts
+++ app/recoils/sample/user/atom.ts
@@ -1,5 +1,18 @@
 import { atom } from 'recoil';
 
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 // atom 생성
 export const userState = atom({
   key: 'userState',
@@ -9,4 +22,5 @@ export const userState = atom({
     username: '슈퍼맨',
     tagList: ['프론트엔드', 'ReactJS', 'NextJS', 'Javascript', 'HTML', 'CSS'],
   },
+  effects: [localStorageEffect('user_info')],
 });
