92fb66c7e1cc329985fd361d65d3c3c5dff2e0b9
huun0424 <huunl91@naver.com>
2024-02-13 15:23:55 +0900

    - feat: GlobalContainer comp 추가 및 layout 적용.

---
+++ app/components/common/GlobalContainer.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode, useState } from 'react';
+import { RecoilRoot } from 'recoil';
+
+interface GlobalContainerProps {
+  children: ReactNode;
+}
+
+const GlobalContainer = ({ children }: GlobalContainerProps) => {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을때 데이터를 refetch
+          refetchOnMount: false, // 데이터가 stale 상태이면 컴포넌트가 마운트될 때 refetch
+          retry: 0, // API 요청 실패시 재시도
+          throwOnError: true, // ErrorBoundary 사용
+        },
+      },
+    }),
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      <RecoilRoot>{children}</RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default GlobalContainer;
