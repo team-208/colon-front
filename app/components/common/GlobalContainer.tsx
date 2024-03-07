553f2f0c9ac4884f6252f2bcc5c55e833df3271a
LeeHeeyun <huunl91@naver.com>
2024-03-07 16:05:11 +0900

    feat: styled components module 추가. (#6)
     - feat: styled components module 추가.

--- app/components/common/GlobalContainer.tsx
+++ app/components/common/GlobalContainer.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 import { RecoilRoot } from 'recoil';
+import StyledComponentsRegistry from './StyledComponentRegistry';
 
 interface GlobalContainerProps {
   children: ReactNode;
@@ -25,7 +26,9 @@ const GlobalContainer = ({ children }: GlobalContainerProps) => {
 
   return (
     <QueryClientProvider client={client}>
-      <RecoilRoot>{children}</RecoilRoot>
+      <StyledComponentsRegistry>
+        <RecoilRoot>{children}</RecoilRoot>
+      </StyledComponentsRegistry>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
