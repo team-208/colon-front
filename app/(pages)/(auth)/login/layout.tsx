d205a5c8852716a6be6d7ed19febf3c63173ccdd
huun0424 <huunl91@naver.com>
2024-03-08 16:31:03 +0900

    - feat: login page 퍼블리싱.

---
+++ app/(pages)/(auth)/login/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import Login from './page';
+
+export const metadata: Metadata = {
+  title: 'CO:LON | 로그인',
+  description: '랜딩 페이지 | desc 설정 필요',
+};
+
+export default Login;
