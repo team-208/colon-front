a800dace1a4eed40778869e6a03a6e420aeb9c48
huun0424 <huunl91@naver.com>
2024-03-08 17:22:21 +0900

    - feat: signup page 퍼블리싱.

---
+++ app/(pages)/(auth)/signup/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next';
+import SignUp from './page';
+
+export const metadata: Metadata = {
+  title: 'CO:LON | 회원가입',
+  description: '회원가입 페이지 | desc 설정 필요',
+};
+
+export default SignUp;
