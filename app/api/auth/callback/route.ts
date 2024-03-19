33caf226af3fb884ba60d04f1da07d9e0d9abdbb
huun0424 <huunl91@naver.com>
2024-03-20 01:07:36 +0900

    - fix: auth/callback api 가입 회원 조회 로직 추가.

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,23 +1,43 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@/app/utils/supabase/server';
+import { isEmpty } from 'lodash';
 
+// TODO: error handling 논의 필요.
+// 공용 에러 페이지로 통일한다면 api단에서 에러 처리.
+// 에러 케이스별 화면 변경이 필요하다면 error code response 후 csr로 처리.
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
-  const next = '/';
 
   const supabase = await createClient(true);
 
-  if (code) {
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      // TODO: 회원가입 여부 판단하기 후 미가입 이용자일 경우 회원가입 페이지로 분기.
-      return NextResponse.redirect(`${origin}${next}`);
-    }
-  }
+  try {
+    if (code) {
+      const { error } = await supabase.auth.exchangeCodeForSession(code);
+      if (error) {
+        throw Error('exchangeCodeForSession error');
+      }
 
-  // 로그인 에러 발생시 redirect
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+      const { error: sessionError } = await supabase.auth.getSession();
+      if (sessionError) {
+        throw Error('get session error');
+      }
+
+      const { data: userInfo, error: userInfoError } = await supabase.from('user_info').select('*');
+      if (userInfoError) {
+        throw Error('user info error');
+      }
+
+      if (isEmpty(userInfo)) {
+        return NextResponse.redirect(`${origin}/signup`);
+      }
+
+      return NextResponse.redirect(`${origin}/`);
+    }
+  } catch (error) {
+    // 로그인 에러 발생시 redirect
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  }
 }
 
 // ref: https://supabase.com/docs/guides/auth/server-side/oauth-with-pkce-flow-for-ssr
