24572cd8c6dd40e71cc44879d396d66ecea4934d
ohdal <yoonhj97@naver.com>
2024-03-20 15:05:24 +0900

    - feat: Comment 컴포넌트 퍼블리싱.

--- app/components/common/Comment.tsx
+++ app/components/common/Comment.tsx
@@ -1,9 +1,64 @@
 'use client';
 
+import { useCallback } from 'react';
+import styled from 'styled-components';
 import { Comment } from '@/app/types/data';
 
-type Props = Comment;
+type Props = Comment & { isDelete?: boolean };
+
+const CommentContainerdiv = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  border-radius: 8px;
+  border: 1px solid #d9d9d9;
+`;
+
+const CommentTextP = styled.p`
+  font-size: 20px;
+  margin-bottom: 15px;
+`;
+
+const CommentPostTitleP = styled.p`
+  font-size: 20px;
+  color: #d9d9d9;
+`;
+
+const CommentButtonLayoutdiv = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  > *:not(:last-child) {
+    margin-right: 4px;
+  }
+`;
+
+const CommentButton = styled.div`
+  display: inline-block;
+  padding: 8px;
+  font-size: 20px;
+  background: transparent;
+`;
 
 export default function Comment(props: Props) {
-  return <div>Comment</div>;
+  const { id, postInfo, comment, isDelete } = props;
+
+  const handleClickDelete = useCallback((id: number) => {
+    console.log('삭제', id);
+  }, []);
+
+  return (
+    <CommentContainerdiv>
+      <CommentTextP>{comment}</CommentTextP>
+      <CommentPostTitleP>{postInfo.title}</CommentPostTitleP>
+
+      {isDelete && (
+        <CommentButtonLayoutdiv>
+          <CommentButton onClick={() => handleClickDelete(id)}>삭제</CommentButton>
+        </CommentButtonLayoutdiv>
+      )}
+    </CommentContainerdiv>
+  );
 }
