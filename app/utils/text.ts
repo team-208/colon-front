d8a066aa9513b4918ea58371c6280fc2f2306f58
ohdal <yoonhj97@naver.com>
2024-03-26 17:30:24 +0900

    - feat: dayjs 의존성 추가 및 게시글, 댓글 생성시간 표시 util 함수 추가

--- app/utils/text.ts
+++ app/utils/text.ts
@@ -1,3 +1,19 @@
+import dayjs from 'dayjs';
 export const cutText = (text: string, length: number, replace: string): string => {
   return text.length > length ? text.substring(0, length) + replace : text;
+};
+
+export const dateText = (date: Date): string => {
+  const cur = new Date();
+  const diff = Math.trunc((cur.getTime() - date.getTime()) / 1000 / 60); // 분단위
+
+  if (diff < 60) {
+    return diff + '분 전';
+  } else if (diff < 60 * 24) {
+    return Math.trunc(diff / 60) + '시간 전';
+  } else if (diff <= 60 * 24 * 7) {
+    return Math.trunc(diff / 60 / 24) + '일 전';
+  } else {
+    return dayjs(date).format('YY/MM/DD');
+  }
 };
