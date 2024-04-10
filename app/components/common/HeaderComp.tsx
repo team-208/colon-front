88c5c60c0e6f714635282bd218f669108d16688c
huun0424 <huunl91@naver.com>
2024-04-10 17:13:52 +0900

    - feat: HeaderComp 로그인 확인용 임시 코드 추가.

--- app/components/common/HeaderComp.tsx
+++ app/components/common/HeaderComp.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import useAuth from '@/app/hooks/useAuth';
 import styled from 'styled-components';
 
 const ContainerHeader = styled.header`
@@ -10,11 +11,18 @@ const ContainerHeader = styled.header`
 `;
 
 const HeaderComp = () => {
+  // TODO: 로그인 확인용 임시 로직
+  const { userInfo } = useAuth();
+
   return (
     <ContainerHeader>
       <h1>아이콘</h1>
       <h1>[page1]</h1>
       <h1>[page2]</h1>
+      <h1>
+        로그인 유저:
+        {userInfo?.user.nick_name ?? ''}
+      </h1>
     </ContainerHeader>
   );
 };
