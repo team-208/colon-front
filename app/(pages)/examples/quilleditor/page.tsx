3d8dd472f20a815250451e73a46525c0d4702862
ohdal <yoonhj97@naver.com>
2024-04-13 14:04:55 +0900

    - feat: QuillEditor 컴포넌트 예제 페이지 작성.

--- app/(pages)/examples/quilleditor/page.tsx
+++ app/(pages)/examples/quilleditor/page.tsx
@@ -1,7 +1,51 @@
-import dynamic from 'next/dynamic';
+'use client';
 
-const QuillEditor = dynamic(() => import('@/app/components/common/QuillEditor'), { ssr: false });
+import { useState } from 'react';
+import { createTextFile } from '@/app/utils/text';
+import QuillEditor from '@/app/components/common/QuillEditor';
 
 export default function QuilleditorPage() {
-  return <QuillEditor />;
+  const [content, setContent] = useState('');
+
+  const handleClickSave = (isTemporary: boolean) => {
+
+    // content text .txt 파일로 변환 storage 저장
+    // directory, file 명 논의하기
+    const file = createTextFile(content, 'userid_date');
+
+    const obj = {
+      title: 'title',
+      content: 'storage_file_url',
+      temporary: isTemporary,
+    };
+
+    // obj 객체 posts DB 저장
+  };
+
+  const setHTML = (v: string) => {
+    setContent(v);
+  };
+
+  return (
+    <>
+      {/* <QuillEditor setHTML={setHTML} initValue="<p>안녕하세요</p>" /> */}
+      <QuillEditor setHTML={setHTML} />
+      <div>
+        <button
+          onClick={() => {
+            handleClickSave(true);
+          }}
+        >
+          임시저장
+        </button>
+        <button
+          onClick={() => {
+            handleClickSave(false);
+          }}
+        >
+          질문하기
+        </button>
+      </div>
+    </>
+  );
 }
