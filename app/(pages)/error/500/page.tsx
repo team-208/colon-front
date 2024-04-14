cb6b4c4fbff9dae308ee424bfddbc3692d44e423
huun0424 <huunl91@naver.com>
2024-04-14 17:04:40 +0900

    - fix: 500 error 페이지 문구 수정

--- app/(pages)/error/500/page.tsx
+++ app/(pages)/error/500/page.tsx
@@ -11,7 +11,7 @@ export default function Error404Page() {
 
   return (
     <main>
-      <h1>페이지를 찾을 수 없어요. :(</h1>
+      <h1>일시적으로 서버에 문제가 발생했어요. :(</h1>
       <button onClick={handleClickHome}>홈으로 돌아가기</button>
     </main>
   );
