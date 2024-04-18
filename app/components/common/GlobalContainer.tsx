8849ba2381ecfe4c9160323d9f87e917ce54d2de
huun0424 <huunl91@naver.com>
2024-04-18 17:24:09 +0900

    - feat: GlobalContainer ThemeProvider 적용.

--- app/components/common/GlobalContainer.tsx
+++ app/components/common/GlobalContainer.tsx
@@ -5,12 +5,18 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 import { RecoilRoot } from 'recoil';
 import StyledComponentsRegistry from './StyledComponentRegistry';
+import { ThemeProvider } from 'styled-components';
+import theme from '@/app/styles/theme';
+import mediaQuery from '@/app/styles/mediaQuery';
 
 interface GlobalContainerProps {
   children: ReactNode;
 }
 
 const GlobalContainer = ({ children }: GlobalContainerProps) => {
+  // states
+  // TODO: theme 적용시 local storage로 computed 되도록 수정 필요.
+  const [isWhiteTheme, setIsWhiteTheme] = useState<boolean>(true);
   const [client] = useState(
     new QueryClient({
       defaultOptions: {
@@ -27,7 +33,9 @@ const GlobalContainer = ({ children }: GlobalContainerProps) => {
   return (
     <QueryClientProvider client={client}>
       <StyledComponentsRegistry>
-        <RecoilRoot>{children}</RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <RecoilRoot>{children}</RecoilRoot>
+        </ThemeProvider>
       </StyledComponentsRegistry>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
