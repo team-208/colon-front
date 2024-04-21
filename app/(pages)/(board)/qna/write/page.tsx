9842745669e34628624ab8758ec674dec9f4b480
ohdal <yoonhj97@naver.com>
2024-04-21 13:29:58 +0900

    - feat: 게시글 수정 페이지 처리 WriteComp.tsx 예제 작성.

--- app/(pages)/(board)/qna/write/page.tsx
+++ app/(pages)/(board)/qna/write/page.tsx
@@ -1,7 +1,13 @@
 import SectionComp from '@/app/components/common/SectionComp';
+import Writecomp from '@/app/components/board/qna/write/WriteComp';
 
 const WritePage = () => {
-  return <SectionComp direction="column">QnA 게시글 작성 페이지</SectionComp>;
+  return (
+    <SectionComp direction="column">
+      QnA 게시글 작성 페이지
+      <Writecomp />
+    </SectionComp>
+  );
 };
 
 export default WritePage;
