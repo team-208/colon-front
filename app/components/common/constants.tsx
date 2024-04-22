36b7a9cf22bc31d7090976f48306976bcd6b610c
huun0424 <huunl91@naver.com>
2024-04-22 22:36:08 +0900

    - feat: JobGroup 관련 constants 추가.

---
+++ app/components/common/constants.tsx
@@ -0,0 +1,12 @@
+export interface JobGroupLabelTypes {
+  [key: string]: string;
+  PLANNING: string;
+  DESIGN: string;
+  DEVELOP: string;
+}
+
+export const JOB_GROUP_LABELS: JobGroupLabelTypes = {
+  PLANNING: '기획',
+  DESIGN: '디자인',
+  DEVELOP: '개발',
+};
