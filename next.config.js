f6bc77bbb0b8bb218877146b0797677620399438
huun0424 <huunl91@naver.com>
2024-04-23 19:22:43 +0900

    - feat: storage image config 설정.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rlwvcjygwndaidplsnnk.supabase.co',
+        pathname: '/storage/v1/object/public/images/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
