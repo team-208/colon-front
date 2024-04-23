206b8471444e22d2146af9bfa63937866b16023e
ohdal <yoonhj97@naver.com>
2024-04-23 13:19:31 +0900

    - feat: HeaderComp.tsx 퍼블리싱.

--- app/components/common/HeaderComp.tsx
+++ app/components/common/HeaderComp.tsx
@@ -1,13 +1,74 @@
 'use client';
 
+import Image from 'next/image';
+import Link from 'next/link';
 import useAuth from '@/app/hooks/useAuth';
 import styled from 'styled-components';
+import logoImg from '../../assets/images/logo.png';
+import icon_bell from '../../assets/images/header/Bell.png';
+import icon_search from '../../assets/images/header/Search.png';
+import { headerMenu } from '@/app/constants/menu';
 
 const ContainerHeader = styled.header`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  background-color: #e0e0e0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   height: 60px;
+  background-color: ${({ theme }) => theme.color.static.light};
+  border-bottom: 1px solid #cbcbcb;
+
+  ${({ theme }) => theme.mediaQuery.mobile} {
+    height: 56px;
+  }
+`;
+
+const ContainerHeaderInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: inherit;
+  padding: 0 170px;
+
+  ${({ theme }) => theme.mediaQuery.mobile} {
+    padding: 0 20px;
+  }
+`;
+
+const LogoContainer = styled.div`
+  position: relative;
+  width: 80px;
+  height: 42px;
+  margin-right: 24px;
+`;
+
+const FlexRowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  &.gap {
+    :not(:last-child) {
+      margin-right: 10px;
+    }
+  }
+`;
+
+const HeaderNav = styled.nav`
+  li {
+    display: inline-flex;
+    height: inherit;
+    ${({ theme }) => theme.font.body3}
+
+    a {
+      text-decoration: none;
+      color: ${({ theme }) => theme.color.label.normal};
+    }
+
+    &:not(:last-child) {
+      margin-right: 10px;
+    }
+  }
 `;
 
 const HeaderComp = () => {
@@ -16,10 +77,33 @@ const HeaderComp = () => {
 
   return (
     <ContainerHeader>
-      <h1>아이콘</h1>
-      <h1>[page1]</h1>
-      <h1>[page2]</h1>
-      <h1>{userInfo?.user?.nick_name ?? ''}</h1>
+      <ContainerHeaderInner>
+        <FlexRowDiv>
+          <LogoContainer>
+            <Link href="/">
+              <Image alt="" src={logoImg} objectFit="contain" fill />
+            </Link>
+          </LogoContainer>
+          <HeaderNav>
+            <ul>
+              {headerMenu.map((v) => (
+                <li key={v.id}>
+                  <Link href={v.route}>{v.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </HeaderNav>
+        </FlexRowDiv>
+
+        {userInfo && (
+          <FlexRowDiv className="gap">
+            <Image alt="" src={icon_search} width={24} height={24} />
+            <Image alt="" src={icon_bell} width={24} height={24} />
+            {/* TODO: profile_url 처리 */}
+            <h1>{userInfo?.user?.nick_name ?? ''}</h1>
+          </FlexRowDiv>
+        )}
+      </ContainerHeaderInner>
     </ContainerHeader>
   );
 };
