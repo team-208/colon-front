e576239988cd5a113f6f2b58f583769a19c2f740
huun0424 <huunl91@naver.com>
2024-05-04 17:59:33 +0900

    - feat: CommentComp Header 추가.

--- app/components/common/CommentComp/index.tsx
+++ app/components/common/CommentComp/index.tsx
@@ -1,6 +1,8 @@
+import Header, { HeaderType } from './Header';
 import Wrapper, { WrapperType } from './Wrapper';
 
-const CommentComp: WrapperType = () => <></>;
+const CommentComp: WrapperType & HeaderType = () => <></>;
 
 CommentComp.Wrapper = Wrapper;
+CommentComp.Header = Header;
 export default CommentComp;
