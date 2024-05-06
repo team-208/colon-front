cbf6fac7f8f8dc3f274f6eb913f07e36b409e902
ohdal <yoonhj97@naver.com>
2024-05-07 01:46:23 +0900

    - feat: constants sorMenuList 추가.

--- app/constants/menu.ts
+++ app/constants/menu.ts
@@ -1,1 +1,10 @@
+type SortMenuList = { id: number; text: string; value: number | string }[];
+
 export const headerMenu = [{ id: 1, text: 'Q&A', route: '/qna' }];
+
+export const sortMenuList: SortMenuList = [
+  { id: 1, text: '최신순', value: 0 },
+  { id: 2, text: '반응순', value: 1 },
+  { id: 3, text: '답변순', value: 2 },
+  { id: 4, text: '스크랩순', value: 3 },
+];
