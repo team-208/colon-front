8201418330ee9fa8cf85b129aed72f38ef13aafd
ohdal <yoonhj97@naver.com>
2024-05-14 13:31:21 +0900

    = feat: TopArea 페이지 적용.

--- app/(pages)/(board)/qna/page.tsx
+++ app/(pages)/(board)/qna/page.tsx
@@ -1,10 +1,23 @@
+'use client';
+
+import styled from 'styled-components';
 import SectionComp from '@/app/components/common/SectionComp';
+import TopArea from '@/app/components/board/qna/TopArea';
+
+const ContainerMain = styled.main`
+  padding: 0 180px;
+  ${({ theme }) => theme.mediaQuery.mobile} {
+    padding: 0;
+  }
+`;
 
 const QnaPage = () => {
   return (
-    <main>
-      <SectionComp direction="column">QnA 게시판 페이지</SectionComp>
-    </main>
+    <ContainerMain>
+      <SectionComp direction="column" padding="0">
+        <TopArea />
+      </SectionComp>
+    </ContainerMain>
   );
 };
 
