e6389af1337c86bd57ea5eb8446fe11790c904db
LeeHeeyun <huunl91@naver.com>
2024-06-04 13:39:18 +0900

    [feature/TASK1-120] 모달 hook comp (#49)
    feat: modal type, recoil atom, key 추가.
    feat: modal hook 추가.
    feat: modal container 추가 및 적용.
    feat: Modal Confirm comp 추가.
    feat: Modal example page 추가.

---
+++ app/recoils/modal/atom.ts
@@ -0,0 +1,19 @@
+import { atom } from 'recoil';
+import { ModalRecoilStateProps } from '@/app/types/modalType';
+import { RECOIL_KEYS } from '../key.d';
+
+export const modalInitValue: ModalRecoilStateProps = {
+  isOpen: false,
+  modalProps: {
+    contents: '',
+    confirmLabel: '확인',
+    cancelLabel: '취소',
+    onConfirm: () => {},
+    onCancel: () => {},
+  },
+};
+
+export const modalState = atom({
+  key: RECOIL_KEYS.modalState,
+  default: modalInitValue,
+});
