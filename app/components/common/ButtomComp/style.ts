94b1586c24e76d10b2c595c86b7bf2c4d6bed9d1
OHDAL <yoonhj97@naver.com>
2024-06-19 14:15:47 +0900

    - fix: ButtonSizeStyle 변경.

--- app/components/common/ButtomComp/style.ts
+++ app/components/common/ButtomComp/style.ts
@@ -4,25 +4,25 @@ export const ButtonSizeStyle = (size: string) => {
   switch (size) {
     case 'lg':
       return css`
-        ${({ theme }) => theme.font.body1};
-        min-width: 160px;
+        ${({ theme }) => theme.font.body1}
+        width: fit-content;
         height: 50px;
         border-radius: 15px;
         padding: 12px 28px;
-      `;
+        `;
     case 'sm':
       return css`
         ${({ theme }) => theme.font.body3}
-        min-width: 128px;
-        height: 36px;
+        width: fit-content;
+        height: 34px;
         border-radius: 12px;
         padding: 8px 20px;
-      `;
+        `;
     case 'md':
-    default:
-      return css`
+      default:
+        return css`
         ${({ theme }) => theme.font.body2}
-        min-width: 140px;
+        width: fit-content;
         height: 40px;
         border-radius: 13px;
         padding: 10px 24px;
