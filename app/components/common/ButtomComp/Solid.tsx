44712decd2e739960d115db3c64d778a2448684a
ohdal <yoonhj97@naver.com>
2024-06-20 14:47:06 +0900

    - fix: ButtonComp Solid 버튼 hover, focus 이벤트 관련 Props 추가.

--- app/components/common/ButtomComp/Solid.tsx
+++ app/components/common/ButtomComp/Solid.tsx
@@ -3,9 +3,17 @@
 import styled, { RuleSet, css } from 'styled-components';
 import { ButtonProps } from './type';
 import { ButtonSizeStyle } from './style';
-interface Props extends ButtonProps {}
+interface Props extends ButtonProps {
+  hoverEffect?: boolean;
+  focusEffect?: boolean;
+}
 
-const SolidButton = styled.button<{ $isActive: boolean; $sizeStyle: RuleSet<object> }>`
+const SolidButton = styled.button<{
+  $isActive: boolean;
+  $hoverEffect: boolean;
+  $focusEffect: boolean;
+  $sizeStyle: RuleSet<object>;
+}>`
   ${({ $sizeStyle }) => $sizeStyle}
   border-radius: 15px;
 
@@ -20,24 +28,44 @@ const SolidButton = styled.button<{ $isActive: boolean; $sizeStyle: RuleSet<obje
           color: ${theme.color.label.normal};
         `}
 
-  &:hover {
-    /* Sementic/Primary/Strong */
-    background: linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), #00a1ff;
-  }
+  ${({ $hoverEffect }) =>
+    $hoverEffect &&
+    css`
+      &:hover {
+        /* Sementic/Primary/Strong */
+        background: linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), #00a1ff;
+      }
+    `}
 
-  &:focus {
-    /* Sementic/Primary/Heavy */
-    background: #0080db;
-    filter: rgba(0, 0, 0, 0.1);
-  }
+
+  ${({ $focusEffect }) =>
+    $focusEffect &&
+    css`
+      &:focus {
+        /* Sementic/Primary/Heavy */
+        background: #0080db;
+        filter: rgba(0, 0, 0, 0.1);
+      }
+    `}
 `;
 
-const Solid = ({ children, text, isActive, onClick, size, ...props }: Props) => {
+const Solid = ({
+  children,
+  text,
+  isActive,
+  hoverEffect = true,
+  focusEffect = true,
+  onClick,
+  size,
+  ...props
+}: Props) => {
   return (
     <SolidButton
       {...props}
       $sizeStyle={ButtonSizeStyle(size ?? 'md')}
       $isActive={isActive}
+      $hoverEffect={hoverEffect}
+      $focusEffect={focusEffect}
       onClick={onClick}
     >
       {children || text}
