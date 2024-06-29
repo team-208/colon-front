7bedde23179d8f40fc4302577aa03dbf89939f7a
ohdal <yoonhj97@naver.com>
2024-06-29 13:51:21 +0900

    - fix: SearchHeader, MypageHeader MobileRenderBox 적용.

--- app/components/mypage/MypageHeader.tsx
+++ app/components/mypage/MypageHeader.tsx
@@ -1,15 +1,62 @@
 'use client';
 
+import Image from 'next/image';
+import styled from 'styled-components';
+import { useRouter } from 'next/navigation';
 import { myPageHeaderState } from '@/app/recoils';
 import { useRecoilValue } from 'recoil';
-import BasicHeader from '../common/BasicHeader';
+import HeaderComp from '../common/HeaderComp';
 import ConfirmHeader from '../common/ConfirmHeader';
+import MobileRenderBox from '../common/HeaderComp/MobileRenderBox';
+import { IMAGE_CDN } from '@/app/constants/externalUrls';
+
+const ContainerFlex = styled(HeaderComp.Container)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FlexRowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
 
 const MyPageHeader = () => {
   const { isModify, onConfirm, onCancel } = useRecoilValue(myPageHeaderState);
 
+  const { back } = useRouter();
+
   return (
-    <>{isModify ? <ConfirmHeader onConfirm={onConfirm} onCancel={onCancel} /> : <BasicHeader />}</>
+    <>
+      {isModify ? (
+        <ConfirmHeader onConfirm={onConfirm} onCancel={onCancel} />
+      ) : (
+        <ContainerFlex>
+          <MobileRenderBox renderMode="visible">
+            <button onClick={back}>
+              <Image
+                alt="더보기 아이콘"
+                src={`${IMAGE_CDN}/icon/Icon_Back.png`}
+                width={24}
+                height={24}
+              />
+            </button>
+          </MobileRenderBox>
+          <MobileRenderBox renderMode="hidden">
+            <FlexRowDiv>
+              <HeaderComp.Logo margin="0 24px 0 0" />
+              <HeaderComp.Navigation />
+            </FlexRowDiv>
+          </MobileRenderBox>
+
+          <FlexRowDiv>
+            <HeaderComp.AlertButton />
+            <HeaderComp.ProfileButton />
+          </FlexRowDiv>
+        </ContainerFlex>
+      )}
+    </>
   );
 };
 
