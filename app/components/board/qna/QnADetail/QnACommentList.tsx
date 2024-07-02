f6d13b16d5cc57c78eded2cd7d330322ee575835
LeeHeeyun <huunl91@naver.com>
2024-07-02 22:15:03 +0900

    [feature/TASK1-159] 댓글 수정 기능 추가 (#73)
    fix: 댓글 조회 컬럼 수정.
    feat: CommentItem 분리 및 Comment header Selector 적용.
    feat: 댓글 수정 퍼블리싱.
    feat: comment put fetch, mutation, route, type 추가.
    fix: comment get order id 추가.
    feat: 댓글 수정 api 연동.

--- app/components/board/qna/QnADetail/QnACommentList.tsx
+++ app/components/board/qna/QnADetail/QnACommentList.tsx
@@ -1,16 +1,12 @@
 'use client';
 
 import useCommentsQuery from '@/app/api/comment/[postId]/queries';
-import { useModifyPostMutation } from '@/app/api/post/[id]/mutations';
-import ButtonComp from '@/app/components/common/ButtomComp';
-import CommentComp from '@/app/components/common/CommentComp';
 import { IMAGE_CDN } from '@/app/constants/externalUrls';
 import useAuth from '@/app/hooks/useAuth';
-import dayjs from 'dayjs';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import React, { useCallback, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
+import CommentItem from './CommentItem';
 
 interface Props {
   postId: string;
@@ -47,75 +43,12 @@ const SelectButton = styled.button`
   }
 `;
 
-const CommentP = styled.p`
-  ${({ theme }) => theme.font.body2};
-  font-weight: 400;
-  color: ${({ theme }) => theme.color.label.normal};
-  margin: 6px 0 10px;
-`;
-
-const commentList = [
-  {
-    id: 1,
-    major: 'DEVELOP',
-    nickname: '사용자 닉네임',
-    updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-    comment:
-      '개발 공부를 하면서 이런 유용한 정보를 얻을 수 있다니 정말 좋아요! 반응 남기고 갑니다~',
-    isNestedComment: false,
-    isSelected: true,
-  },
-  {
-    id: 2,
-    major: 'DEVELOP',
-    nickname: '사용자 닉네임',
-    updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-    comment:
-      '개발 공부를 하면서 이런 유용한 정보를 얻을 수 있다니 정말 좋아요! 반응 남기고 갑니다~',
-    isNestedComment: true,
-  },
-  {
-    id: 3,
-    major: 'DEVELOP',
-    nickname: '사용자 닉네임',
-    updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-    comment:
-      '개발 공부를 하면서 이런 유용한 정보를 얻을 수 있다니 정말 좋아요! 반응 남기고 갑니다~',
-    isNestedComment: false,
-  },
-];
-
-const FooterBoxDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const ChoiceButton = styled(ButtonComp.Solid)`
-  padding: 4px 10px;
-  height: 26px;
-  border-radius: 8px;
-`;
-
-const emojis = {
-  thumbsUp: 2,
-  smilingHeart: 4,
-  heart: 0,
-};
-
 const QnACommentList = ({ postId, acceptedCommentId, postAuthor }: Props) => {
   const { data } = useCommentsQuery(postId);
-  const { mutateAsync } = useModifyPostMutation();
 
   const { userInfo } = useAuth();
-  const { refresh } = useRouter();
 
   const isAuthor = useMemo(() => postAuthor === userInfo?.user.nick_name, [userInfo]);
-
-  const handleClickChoice = useCallback(async (commentId: number) => {
-    await mutateAsync({ id: parseInt(postId), status: 'COMPLETE', accept_comment_id: commentId });
-    refresh();
-  }, []);
 
   return (
     <ConatinerDiv>
@@ -145,48 +78,37 @@ const QnACommentList = ({ postId, acceptedCommentId, postAuthor }: Props) => {
             nestedComments,
             author_major,
           }) => {
+            const isAuthorComment = userInfo?.user.nick_name === author_nickname;
             return (
               <React.Fragment key={`comment-item-${id}`}>
                 <li>
-                  <CommentComp.Wrapper isNestedComment={!!original_comment}>
-                    <CommentComp.Header
-                      major={author_major}
-                      nickname={author_nickname}
-                      updatedAt={updated_at || created_at}
-                      isSelected={acceptedCommentId === id}
-                    />
-                    <CommentP>{comment}</CommentP>
-                    <FooterBoxDiv>
-                      <CommentComp.Emojis emojis={emojis} />
-                      {isAuthor && !acceptedCommentId && (
-                        <ChoiceButton isActive onClick={() => handleClickChoice(id)}>
-                          글쓴이 채택
-                        </ChoiceButton>
-                      )}
-                    </FooterBoxDiv>
-                  </CommentComp.Wrapper>
+                  <CommentItem
+                    postId={postId}
+                    commentId={id}
+                    isNestedComment={!!original_comment}
+                    authorMajor={author_major}
+                    authorNickName={author_nickname}
+                    updatedAt={updated_at || created_at}
+                    isSelected={acceptedCommentId === id}
+                    comment={comment}
+                    isVisibleChoice={isAuthor && !isAuthorComment && !acceptedCommentId}
+                  />
                 </li>
                 {nestedComments?.map((item, idx) => {
-                  const isAuthorComment = userInfo?.user.nick_name === item.author_nickname;
+                  const isAuthorNestedComment = userInfo?.user.nick_name === item.author_nickname;
                   return (
                     <li key={`nested-comment-item-${item.id}`}>
-                      <CommentComp.Wrapper isNestedComment={true}>
-                        <CommentComp.Header
-                          major={author_major}
-                          nickname={item.author_nickname}
-                          updatedAt={item.updated_at || item.created_at}
-                          isSelected={acceptedCommentId === item.id}
-                        />
-                        <CommentP>{item.comment}</CommentP>
-                        <FooterBoxDiv>
-                          <CommentComp.Emojis emojis={emojis} />
-                          {isAuthor && !isAuthorComment && !acceptedCommentId && (
-                            <ChoiceButton isActive onClick={() => handleClickChoice(item.id)}>
-                              글쓴이 채택
-                            </ChoiceButton>
-                          )}
-                        </FooterBoxDiv>
-                      </CommentComp.Wrapper>
+                      <CommentItem
+                        postId={postId}
+                        commentId={item.id}
+                        isNestedComment
+                        authorMajor={item.author_major}
+                        authorNickName={item.author_nickname}
+                        updatedAt={item.updated_at || item.created_at}
+                        isSelected={acceptedCommentId === item.id}
+                        comment={item.comment}
+                        isVisibleChoice={isAuthor && !isAuthorNestedComment && !acceptedCommentId}
+                      />
                     </li>
                   );
                 })}
--- app/components/common/CommentComp/Wrapper.tsx
+++ app/components/common/CommentComp/Wrapper.tsx
@@ -8,16 +8,19 @@ import styled from 'styled-components';
 interface Props {
   className?: string;
   isNestedComment?: boolean;
+  isModify?: boolean;
   children: ReactNode;
 }
 
-const ContainerDiv = styled.div<{ $isNestedComment: boolean }>`
+const ContainerDiv = styled.div<{ $isNestedComment: boolean; $isModify: boolean }>`
   display: flex;
   padding: 20px;
   width: 100%;
 
-  background-color: ${({ $isNestedComment, theme }) =>
-    $isNestedComment ? theme.color.background.alternative : theme.color.background.normal};
+  background-color: ${({ $isNestedComment, $isModify, theme }) =>
+    $isNestedComment || $isModify
+      ? theme.color.background.alternative
+      : theme.color.background.normal};
 
   ${({ theme }) => theme.mediaQuery.mobile} {
   }
@@ -31,9 +34,9 @@ const ChildrenDiv = styled.div`
   width: 100%;
 `;
 
-const Wrapper = ({ className, isNestedComment, children }: Props) => {
+const Wrapper = ({ className, isNestedComment, children, isModify }: Props) => {
   return (
-    <ContainerDiv className={className} $isNestedComment={!!isNestedComment}>
+    <ContainerDiv className={className} $isNestedComment={!!isNestedComment} $isModify={!!isModify}>
       {isNestedComment && (
         <NestedIconImage
           alt="대댓글 아이콘"
