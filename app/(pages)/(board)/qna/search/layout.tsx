03b2dece8db5b3db4ceb55dc89d70acfa35fb991
OHDAL <yoonhj97@naver.com>
2024-07-08 22:29:03 +0900

    - feat: 검색페이지 routing.

---
+++ app/(pages)/(board)/qna/search/layout.tsx
@@ -0,0 +1,9 @@
+import QuestionSearchPage from './page';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'CO:LON | QnA 검색 페이지',
+    description: 'QnA 검색 페이지 | desc 설정 필요',
+  };
+
+export default QuestionSearchPage;
---
+++ app/components/board/qna/search/QnaSerachList.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
+const QnaSerachList = () => {
+  const searchParams = useSearchParams();
+  const param = searchParams.get('word');
+
+  return <div>{param}</div>;
+};
+
+export default QnaSerachList;
