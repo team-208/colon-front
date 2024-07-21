b7f2162270b267f09545d0c2ccde0e725f61b6e6
LeeHeeyun <huunl91@naver.com>
2024-07-21 19:53:35 +0900

    [TASK1-207] 댓글 / 대댓글 입력창 하나만 노출 되도록 수정
     - feat: 대댓글 입력, 댓글 입력 중 1개만 노출되도록 조건 수정. (#88)

--- app/components/board/qna/QnADetail/QnACommentList.tsx
+++ app/components/board/qna/QnADetail/QnACommentList.tsx
@@ -8,6 +8,8 @@ import CommentItem from './CommentItem';
 import Selector from '@/app/components/common/Selector';
 import { isEmpty } from 'lodash';
 import { GetCommentsResponseItem } from '@/app/api/comment/[postId]/type';
+import Section from '@/app/components/common/Section';
+import QnACommentWrite from './QnACommentWrite';
 
 interface Props {
   postId: string;
@@ -34,6 +36,10 @@ const TitleP = styled.p`
 
 const QnACommentList = ({ postId, acceptedCommentId, postAuthor }: Props) => {
   const [commentList, setCommentList] = useState<GetCommentsResponseItem[]>([]);
+  const [openedNestedComment, setOpenedNestedComment] = useState<{
+    isOpen: boolean;
+    commentId: number;
+  }>({ isOpen: false, commentId: -1 });
 
   const { data } = useCommentsQuery(postId);
 
@@ -58,6 +64,13 @@ const QnACommentList = ({ postId, acceptedCommentId, postAuthor }: Props) => {
     [commentList],
   );
 
+  const handleChangeNestedCommentVisible = useCallback(
+    (isOpen: boolean, commentId: number) => {
+      setOpenedNestedComment({ isOpen, commentId });
+    },
+    [setOpenedNestedComment],
+  );
+
   useEffect(() => {
     if (data && !isEmpty(data) && isEmpty(commentList)) {
       setCommentList(data);
@@ -65,83 +78,109 @@ const QnACommentList = ({ postId, acceptedCommentId, postAuthor }: Props) => {
   }, [data]);
 
   return (
-    <ConatinerDiv>
-      <FilterDiv>
-        <TitleP>댓글</TitleP>
-        <Selector defaultOption={{ idx: 0, text: '정렬' }}>
-          <Selector.Option
-            idx={0}
-            text="시간순"
-            clickEvent={() => {
-              handleSortList('updated');
-            }}
-          />
-          <Selector.Option
-            idx={1}
-            text="반응순"
-            clickEvent={() => {
-              handleSortList('reaction');
-            }}
-          />
-        </Selector>
-      </FilterDiv>
+    <>
+      <Section direction="column" padding="0">
+        <ConatinerDiv>
+          <FilterDiv>
+            <TitleP>댓글</TitleP>
+            <Selector defaultOption={{ idx: 0, text: '정렬' }}>
+              <Selector.Option
+                idx={0}
+                text="시간순"
+                clickEvent={() => {
+                  handleSortList('updated');
+                }}
+              />
+              <Selector.Option
+                idx={1}
+                text="반응순"
+                clickEvent={() => {
+                  handleSortList('reaction');
+                }}
+              />
+            </Selector>
+          </FilterDiv>
 
-      <ul>
-        {commentList?.map(
-          ({
-            id,
-            author_nickname,
-            created_at,
-            updated_at,
-            comment,
-            original_comment,
-            nestedComments,
-            author_major,
-            reaction_count,
-          }) => {
-            const isAuthorComment = userInfo?.user?.nick_name === author_nickname;
-            return (
-              <React.Fragment key={`comment-item-${id}`}>
-                <li>
-                  <CommentItem
-                    postId={postId}
-                    commentId={id}
-                    isNestedComment={!!original_comment}
-                    authorMajor={author_major}
-                    authorNickName={author_nickname}
-                    updatedAt={updated_at || created_at}
-                    isSelected={acceptedCommentId === id}
-                    comment={comment}
-                    isVisibleChoice={isAuthor && !isAuthorComment && !acceptedCommentId}
-                    likeCount={reaction_count}
-                    nestedCommentCount={nestedComments?.length ?? 0}
-                  />
-                </li>
-                {nestedComments?.map((item, idx) => {
-                  const isAuthorNestedComment = userInfo?.user?.nick_name === item.author_nickname;
-                  return (
-                    <li key={`nested-comment-item-${item.id}`}>
+          <ul>
+            {commentList?.map(
+              ({
+                id,
+                author_nickname,
+                created_at,
+                updated_at,
+                comment,
+                original_comment,
+                nestedComments,
+                author_major,
+                reaction_count,
+              }) => {
+                const isAuthorComment = userInfo?.user?.nick_name === author_nickname;
+                return (
+                  <React.Fragment key={`comment-item-${id}`}>
+                    <li>
                       <CommentItem
                         postId={postId}
-                        commentId={item.id}
-                        isNestedComment
-                        authorMajor={item.author_major}
-                        authorNickName={item.author_nickname}
-                        updatedAt={item.updated_at || item.created_at}
-                        isSelected={acceptedCommentId === item.id}
-                        comment={item.comment}
-                        isVisibleChoice={isAuthor && !isAuthorNestedComment && !acceptedCommentId}
-                        likeCount={item.reaction_count}
+                        commentId={id}
+                        isNestedComment={!!original_comment}
+                        authorMajor={author_major}
+                        authorNickName={author_nickname}
+                        updatedAt={updated_at || created_at}
+                        isSelected={acceptedCommentId === id}
+                        comment={comment}
+                        isVisibleChoice={isAuthor && !isAuthorComment && !acceptedCommentId}
+                        likeCount={reaction_count}
+                        nestedCommentCount={nestedComments?.length ?? 0}
+                        isOpenNestedCommentWrite={
+                          openedNestedComment.isOpen && openedNestedComment.commentId === id
+                        }
+                        onChangeNestedCommentVisible={(isOpen) => {
+                          handleChangeNestedCommentVisible(isOpen, id);
+                        }}
                       />
                     </li>
-                  );
-                })}
-              </React.Fragment>
-            );
-          },
-        )}
-      </ul>
-    </ConatinerDiv>
+                    {nestedComments?.map((item, idx) => {
+                      const isAuthorNestedComment =
+                        userInfo?.user?.nick_name === item.author_nickname;
+                      return (
+                        <li key={`nested-comment-item-${item.id}`}>
+                          <CommentItem
+                            postId={postId}
+                            commentId={item.id}
+                            isNestedComment
+                            authorMajor={item.author_major}
+                            authorNickName={item.author_nickname}
+                            updatedAt={item.updated_at || item.created_at}
+                            isSelected={acceptedCommentId === item.id}
+                            comment={item.comment}
+                            isVisibleChoice={
+                              isAuthor && !isAuthorNestedComment && !acceptedCommentId
+                            }
+                            likeCount={item.reaction_count}
+                            isOpenNestedCommentWrite={
+                              openedNestedComment.isOpen &&
+                              openedNestedComment.commentId === item.id
+                            }
+                            onChangeNestedCommentVisible={(isOpen) => {
+                              handleChangeNestedCommentVisible(isOpen, item.id);
+                            }}
+                          />
+                        </li>
+                      );
+                    })}
+                  </React.Fragment>
+                );
+              },
+            )}
+          </ul>
+        </ConatinerDiv>
+      </Section>
+
+      {!openedNestedComment.isOpen && (
+        <Section direction="column" padding="0">
+          <QnACommentWrite postId={postId} />
+        </Section>
+      )}
+    </>
   );
 };
 
