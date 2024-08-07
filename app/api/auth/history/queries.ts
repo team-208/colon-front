460aaf493873d77742df5d20c6dbc057b4abffa6
huun0424 <huunl91@naver.com>
2024-08-07 21:51:55 +0900

    fix: build fail fix

--- app/api/auth/history/queries.ts
+++ app/api/auth/history/queries.ts
@@ -1,12 +1,16 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import QUERY_KEYS from '../../queryKeys';
 import { GetHistoryQuery } from './type';
 import { fetchGetHistory } from './fetch';
+import supabaseClient from '@/app/utils/supabase/client';
 
 const useHistoryQuery = ({ historyType }: GetHistoryQuery) => {
-  return useSuspenseQuery({
+  const { auth } = supabaseClient;
+
+  return useQuery({
     queryKey: QUERY_KEYS.historyList({ historyType }),
     queryFn: () => fetchGetHistory({ historyType }),
+    enabled: !!auth.getSession(),
   });
 };
 
