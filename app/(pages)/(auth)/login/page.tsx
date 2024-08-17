8e78146aa32c278a64f1c505af9fee235261fd4d
LeeHeeyun <huunl91@naver.com>
2024-08-17 22:35:49 +0900

    [feature/TASK1-170] 랜딩, 로그인 페이지 디자인 변경 - desktop (#96)
    feat: login 화면 desktop 퍼블리싱
    feat: 랜딩화면 desktop 퍼블리싱

--- app/(pages)/(auth)/login/page.tsx
+++ app/(pages)/(auth)/login/page.tsx
@@ -7,6 +7,7 @@ import { getHost } from '@/app/utils/host';
 import Image from 'next/image';
 import logo_kakao from '../../../assets/images/home/logo_kakao.png';
 import styled from 'styled-components';
+import { IMAGE_CDN } from '@/app/constants/externalUrls';
 
 const ContainerMain = styled.main`
   display: flex;
@@ -15,38 +16,87 @@ const ContainerMain = styled.main`
   max-width: 960px;
 `;
 
+const TitleImageBoxDiv = styled.div`
+  position: absolute;
+  left: 50%;
+  top: ${({ theme }) => theme.heightSizes.header.desktop}px;
+  background: linear-gradient(360deg, #78ceff 2.14%, #e2f4ff 81.03%);
+  width: 100vw;
+  height: 350px;
+  transform: translateX(-50%);
+
+  & > div {
+    max-width: 1536px;
+    margin: 0 auto;
+    position: relative;
+    width: 100%;
+    height: inherit;
+    overflow: hidden;
+  }
+`;
+
+const TitleImage1 = styled(Image)`
+  position: absolute;
+  top: -280px;
+  left: -11px;
+`;
+const TitleImage2 = styled(Image)`
+  position: absolute;
+  top: 223px;
+  left: 292px;
+  transform: rotate(107deg);
+`;
+const TitleImage3 = styled(Image)`
+  position: absolute;
+  top: -25px;
+  right: 50px;
+  transform: rotate(240deg);
+`;
+
+const TitleWrapperDiv = styled.div`
+  z-index: 1;
+`;
+
 const TitleP = styled.h1`
-  margin: 48px 0 32px;
-  background: ${({ theme }) => theme.color.gradient.normal};
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-  white-space: pre-line;
-  text-align: center;
-  ${({ theme }) => theme.font.display3};
+  color: ${({ theme }) => theme.color.label.normal};
+  ${({ theme }) => theme.font.display4};
 
   ${({ theme }) => theme.mediaQuery.mobile} {
     ${({ theme }) => theme.font.display4};
   }
 `;
 
-const TempImageRegionDiv = styled.div`
-  height: 266px;
-  width: 100%;
-  background-color: #e5e5e5;
-  border-radius: 12px;
+const TitleDescP = styled.p`
+  ${({ theme }) => theme.font.heading2};
+  font-weight: 400;
+  margin-top: 16px;
+  white-space: pre-wrap;
+  text-align: center;
 `;
 
 const DescUl = styled.ul`
   display: flex;
   margin-top: 56px;
+  padding: 0 8px;
+  width: 100vw;
+  height: 18vw;
+  cursor: pointer;
+
+  ${({ theme }) => theme.mediaQuery.tablet} {
+    padding: 0 40px;
+  }
 
   ${({ theme }) => theme.mediaQuery.mobile} {
+    padding: 0 20px;
     flex-direction: column;
+    height: auto;
   }
 `;
 
 const DescItemLi = styled.li`
+  border-radius: 15px;
+  overflow: hidden;
+  position: relative;
   color: ${({ theme }) => theme.color.label.normal};
   text-align: center;
 
@@ -54,32 +104,12 @@ const DescItemLi = styled.li`
     margin-right: 32px;
   }
 
-  & > p:first-of-type {
-    ${({ theme }) => theme.font.heading1};
-  }
-
-  & > p:first-of-type > span {
-    color: ${({ theme }) => theme.color.primary.normal};
-  }
-
-  & > p:last-of-type {
-    margin-top: 8px;
-    ${({ theme }) => theme.font.body1};
-    opacity: 0.5;
-    white-space: pre-line;
-  }
-
-  /* TODO: 임시 div */
-  & > div {
-    margin-top: 12px;
-    width: 100%;
-    height: 140px;
-    background-color: #f9f9f9;
-    border-radius: 15px;
-  }
-
   ${({ theme }) => theme.mediaQuery.mobile} {
     &:not(:last-of-type) {
+      margin-right: 0;
+    }
+
+    &:not(:first-of-type) {
       margin: 32px 0 0 0;
     }
 
@@ -89,10 +119,48 @@ const DescItemLi = styled.li`
   }
 `;
 
-const LoginTitleP = styled.p`
-  ${({ theme }) => theme.font.title2};
-  color: ${({ theme }) => theme.color.label.normal};
-  text-align: center;
+const ImageBoxDiv = styled.div`
+  position: relative;
+  border-radius: 15px;
+  overflow: hidden;
+  width: 340px;
+  height: 230px;
+
+  ${({ theme }) => theme.mediaQuery.tablet} {
+    width: 32vw;
+    height: 18vw;
+  }
+
+  ${({ theme }) => theme.mediaQuery.mobile} {
+    width: 100vw;
+    height: 60vw;
+  }
+`;
+
+const DescBoxDiv = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > p {
+    ${({ theme }) => theme.font.title2};
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & :hover {
+    backdrop-filter: blur(5px);
+    background-color: ${({ theme }) => `${theme.color.static.dark}4f`};
+    color: ${({ theme }) => theme.color.static.light};
+  }
 `;
 
 const KakaoLoginButton = styled.button`
@@ -102,7 +170,7 @@ const KakaoLoginButton = styled.button`
   align-items: center;
   width: 300px;
   height: 45px;
-  margin: 32px auto 0;
+  margin: 46px auto 0;
   background-color: #ffde09;
   border-radius: 6px;
   font-size: 15px;
@@ -116,45 +184,23 @@ const KakaoLoginButton = styled.button`
   }
 `;
 
-const SignUpButton = styled.button`
-  margin: 12px auto 0;
-  background-color: #c3c3c3;
-  border-radius: 4px;
-  padding: 14px 24px;
-  width: 300px;
-
-  ${({ theme }) => theme.mediaQuery.mobile} {
-    width: 272px;
-  }
+const DescTitleP = styled.p`
+  ${({ theme }) => theme.font.title2};
+  color: ${({ theme }) => theme.color.label.normal};
+  text-align: center;
 `;
 
 const DESC_ITEMS = [
   {
-    title: (
-      <p>
-        <span>소통</span>
-        {` 역량 성장`}
-      </p>
-    ),
+    title: '소통 역량 성장',
     desc: `여러 직군과 자유로운 소통으로 직군 간 이해도를\n높여 소통 역량을 향상시킬 수 있어요.`,
   },
   {
-    title: (
-      <p>
-        <span>간접</span>
-        {` 경험`}
-      </p>
-    ),
+    title: '간접 경험',
     desc: `서로의 협업 경험을 공유하여 여러 사례들을\n간접적으로 경험할 수 있어요.`,
   },
   {
-    title: (
-      <p>
-        {`관점을 `}
-        <span>넓힐 수 있는</span>
-        {` 기회`}
-      </p>
-    ),
+    title: '관점을 넓힐 수 있는 기회',
     desc: `다른 직군과 소통을 통해 서로의 입장을 이해하여\n관점을 넓힐 수 있는 기회를 제공해요.`,
   },
 ];
@@ -170,39 +216,62 @@ export default function Login() {
     login(`${host}/api/auth/callback`);
   };
 
-  const handleClickSignUp = () => {
-    login(`${host}/signup`);
-  };
-
   return (
     <ContainerMain>
       <BasicHeader />
-      <Section direction="column" padding="0 20px">
-        <TitleP>{`풀리지 않던 궁금증을\n해소하는 곳`}</TitleP>
+      <Section direction="column" padding="84px 0 58px">
+        <TitleImageBoxDiv>
+          <div>
+            <TitleImage1
+              alt="상단 이미지1"
+              src={`${IMAGE_CDN}/login/login-bg-1-240814.png`}
+              width={540}
+              height={540}
+            />
+            <TitleImage2
+              alt="상단 이미지2"
+              src={`${IMAGE_CDN}/login/login-bg-2-240814.png`}
+              width={205}
+              height={205}
+            />
+            <TitleImage3
+              alt="상단 이미지3"
+              src={`${IMAGE_CDN}/login/login-bg-3-240814.png`}
+              width={400}
+              height={400}
+            />
+          </div>
+        </TitleImageBoxDiv>
 
-        <TempImageRegionDiv />
+        <TitleWrapperDiv>
+          <TitleP>{`풀리지 않던 궁금증을 해소하는 곳`}</TitleP>
+          <TitleDescP>{`협업이 중요한 IT 직군이지만, 실무 경험이 없어서 고민이라면?\nCO:LON에서 편하게 물어보세요.`}</TitleDescP>
+
+          <KakaoLoginButton onClick={handleClickKakao}>
+            <Image alt="" src={logo_kakao} width={18} height={18} />
+            카카오 로그인
+          </KakaoLoginButton>
+        </TitleWrapperDiv>
+      </Section>
+
+      <Section direction="column" padding="76px 20px 143px">
+        <DescTitleP>CO:LON에서 얻을 수 있는 것들</DescTitleP>
 
         <DescUl>
           {DESC_ITEMS.map((item, idx) => (
             <DescItemLi key={`desc-item-${idx}`}>
-              {item.title}
-              <p>{item.desc}</p>
-              {/* TODO: 임시 div */}
-              <div />
+              <ImageBoxDiv>
+                <Image alt="" src={`${IMAGE_CDN}/login/login-desc-${idx + 1}.png`} fill />
+              </ImageBoxDiv>
+
+              <DescBoxDiv>
+                <p>{item.title}</p>
+
+                {/* <p>{item.desc}</p> */}
+              </DescBoxDiv>
             </DescItemLi>
           ))}
         </DescUl>
-      </Section>
-
-      <Section direction="column" padding="64px 20px 166px">
-        <LoginTitleP>지금 바로 함께하세요!</LoginTitleP>
-        {/* TODO: 디자인 가이드 배포 후, 공용 모듈화 필요 */}
-        <KakaoLoginButton onClick={handleClickKakao}>
-          <Image alt="" src={logo_kakao} width={18} height={18} />
-          카카오 로그인
-        </KakaoLoginButton>
-
-        <SignUpButton onClick={handleClickSignUp}>회원가입</SignUpButton>
       </Section>
     </ContainerMain>
   );
--- app/components/common/Section.tsx
+++ app/components/common/Section.tsx
@@ -9,18 +9,24 @@ interface Props {
   direction: Direction;
   children: ReactNode;
   padding?: string;
+  margin?: string;
 }
 
-const Container = styled.section<{ $dir: Direction; $padding: string }>`
+const Container = styled.section<{ $dir: Direction; $padding: string; $margin: string }>`
   display: flex;
   flex-direction: ${({ $dir }) => $dir};
   padding: ${({ $padding }) => $padding};
+  margin: ${({ $margin }) => $margin};
+
+  ${({ theme }) => theme.mediaQuery.tablet} {
+    max-width: 960px;
+  }
 `;
 
 const Section = (props: Props) => {
-  const { direction, children, padding } = props;
+  const { direction, children, padding, margin } = props;
   return (
-    <Container $dir={direction} $padding={padding ?? '16px'}>
+    <Container $dir={direction} $padding={padding ?? '16px'} $margin={margin ?? '0'}>
       {children}
     </Container>
   );
