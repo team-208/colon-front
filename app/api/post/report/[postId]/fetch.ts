52edb7bd6c96ad00bcd7fee30e047e922849467a
huun0424 <huunl91@naver.com>
2024-12-15 17:27:03 +0900

    - fix: 오탈자 수정.

--- app/api/post/report/[postId]/fetch.ts
+++ app/api/post/report/[postId]/fetch.ts
@@ -1,7 +1,7 @@
 import { GetReportResponse } from './type';
 
 export const fetchGetReport = async (postId: number) => {
-  const res = await fetch(`/api/repost/${postId}`);
+  const res = await fetch(`/api/report/${postId}`);
   const jsonData = (await res.json()) as GetReportResponse;
   return jsonData;
 };
