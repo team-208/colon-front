98e944c1ad49134b85c2be922193e0c735e01d09
huun0424 <huunl91@naver.com>
2025-02-24 03:12:58 +0900

    Feat: 신고 모달 html 작업

--- app/components/common/Icon/svg/index.ts
+++ app/components/common/Icon/svg/index.ts
@@ -1,4 +1,6 @@
 import icArrow from './ic-arrow.svg';
 import icBubble from './ic-bubble.svg';
+import icRadioInactive from './ic-radio-inactive.svg';
+import icNormalCaretDown from './ic-normal-caret-down.svg';
 
-export { icArrow, icBubble };
+export { icArrow, icBubble, icRadioInactive, icNormalCaretDown };
