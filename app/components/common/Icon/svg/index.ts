8a138157897eace9762c47b5c832175e347fe144
huun0424 <huunl91@naver.com>
2025-03-04 19:33:15 +0900

    feat: radio icon 추가.

--- app/components/common/Icon/svg/index.ts
+++ app/components/common/Icon/svg/index.ts
@@ -1,6 +1,7 @@
 import icArrow from './ic-arrow.svg';
 import icBubble from './ic-bubble.svg';
 import icRadioInactive from './ic-radio-inactive.svg';
+import icRadioActive from './ic-radio-active.svg';
 import icNormalCaretDown from './ic-normal-caret-down.svg';
 
-export { icArrow, icBubble, icRadioInactive, icNormalCaretDown };
+export { icArrow, icBubble, icRadioInactive, icRadioActive, icNormalCaretDown };
