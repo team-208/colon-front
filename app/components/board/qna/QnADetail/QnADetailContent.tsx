a8ca5ff12c7df187f086357791bf48709f178d4f
huun0424 <huunl91@naver.com>
2025-03-10 19:40:29 +0900

    - feat: 신고하기 hook 분리 및 댓글 신고하기 적용.

--- app/components/board/qna/QnADetail/QnADetailContent.tsx
+++ app/components/board/qna/QnADetail/QnADetailContent.tsx
@@ -16,8 +16,7 @@ import useUserReactionsQuery from '@/app/api/auth/user/reactions/queries';
 import ButtonComp from '@/app/components/common/ButtomComp';
 import useModal from '@/app/hooks/useModal';
 import QnAReportModal from '../QnAReportModal';
-import { useInsertPostReportMutation } from '@/app/api/report/mutations';
-import { isEmpty } from 'lodash';
+import useReport from '@/app/hooks/useReport';
 
 // TODO: post api response로 interface 수정 필요.
 interface Props {
@@ -159,11 +158,11 @@ const QnADetailContent = ({ post }: Props) => {
   const { data: userReactions } = useUserReactionsQuery();
 
   const { mutateAsync } = useDeletePostMutation();
-  const { mutateAsync: insertPostReport } = useInsertPostReportMutation();
 
   const { push, replace } = useRouter();
   const { userInfo } = useAuth();
   const { openModal, closeModal } = useModal();
+  const { requestReport } = useReport();
 
   const isComplete = useMemo(() => status === 'COMPLETE' && accept_comment_id?.length > 0, []);
   const isScrap = useMemo(
@@ -178,26 +177,8 @@ const QnADetailContent = ({ post }: Props) => {
 
   const handleReport = () => {
     const handleConfirm = async (reason: string) => {
-      if (isEmpty(userInfo)) {
-        window.alert('로그인 후 이용할 수 있습니다.');
-      }
-
-      const { success } = await insertPostReport({
-        postId: id,
-        userNickname: userInfo?.user.nick_name ?? '',
-        status: 'REGISTERED',
-        reason,
-      });
-
-      if (!success) {
-        window.alert('신고하기에 실패하였습니다. 잠시 후 다시 시도해 주세요.');
-        return;
-      }
-
+      requestReport({ postId: id, reason });
       closeModal();
-      window.alert(
-        `신고가 완료되었습니다. 빠른 시일 내로 처리할게요.\n원활한 이야기 공간을 만드는 데 도움 주셔서 감사합니다!`,
-      );
     };
 
     openModal({
